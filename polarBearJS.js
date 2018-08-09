44ad5dc139eb4bcf1c4913b984d2c3b90dbbf7a0
Anna Reid <annareid@Annas-MacBook-Pro.local>
2018-08-09 15:32:59 -0700

    message

--- polarBearJS.js
+++ polarBearJS.js
@@ -1,4 +1,5 @@
 
+//starting functions
 var strings = [
   "The polar bear is the Arctic's top predator."
   "The scientific name of the polar bear is Ursus maritimus."
@@ -6,7 +7,3 @@ var strings = [
   "Two third of all polar bears live in Canadian land."
   "Polar bears rely on sea ice that allows them to have access to seals, their main food source. Sea ice is melting due to climate change, which is a major threat to the bears."
 ]
-
-function gameGo(event){
-  //the code that makes the game run
-}
