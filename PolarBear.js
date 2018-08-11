10fd59108dfcf1576e28f40c343f2b9d1f08e34c
Anna Reid <annareid@Annas-MacBook-Pro.local>
2018-08-11 12:20:26 -0700

    the uploaded code is broken but the bear moves??

--- PolarBear.js
+++ PolarBear.js
@@ -3,8 +3,11 @@ var paddleWidth = 75;
 var rightPressed = false;
 var leftPressed = false;
 var ctx;
-
 var bear;
+var pbImage = false;
+
+//the animation of the bear to move
+
 
 //starting functions
 var strings = [
@@ -14,6 +17,44 @@ var strings = [
   "Two third of all polar bears live in Canadian land.",
   "Polar bears rely on sea ice that allows them to have access to seals, their main food source. Sea ice is melting due to climate change, which is a major threat to the bears."
 ]
+
+//the animate bear function
+window.onload = function() {
+   bearAnimation();
+ }
+
+ function bearAnimation(){
+      var ctx = document.getElementById("myCanvas").getContext('2d');
+      var myImage = new Image();
+      myImage.src = "polarbearREALSS.png";
+      myImage.addEventListener("load", loadImage, false);
+
+      function loadImage(e) {
+        animate();
+      }
+
+      var shift = 0;
+      var frameWidth = 150;
+      var frameHeight = 116;
+      var totalFrames = 7;
+      var currentFrame = 0;
+
+      function animate() {
+        ctx.clearRect(30, 20, 150, 116);
+        ctx.drawImage(myImage, shift, 0, frameWidth, frameHeight,
+                          30, 300, frameWidth, frameHeight);
+
+        shift += frameWidth + 1;
+
+        if (currentFrame == totalFrames) {
+          shift = 0;
+          currentFrame = 0;
+        }
+        currentFrame++;
+        requestAnimationFrame(animate);
+      }
+    }
+//where the new animation code ends
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -48,15 +89,25 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+
+
+
 //draws the bear initially when the game starts
-function Bear(x, y){
+/*function Bear(x, y){
   this.x = x;
   this.y = y;
   this.image = new Image ();
-  this.image.src = "PolarBearDraw.png";
+  this.image.src = "PolarBearDraw1.png";
   this.drawBear = function(){
     var img = document.getElementById("bearwalk1");
     ctx.drawImage(img, this.x, this.y,194.12,150);
+    this.moveBearUp = function() {
+      this.y = this.y - 100;
+    }
+    this.moveBearDown = function(){
+      this.y = this.y + 100;
+    }
+
   }
 }
 function startBear(){
@@ -64,6 +115,10 @@ function startBear(){
   bear.drawBear();
 
 }
+
+//function animateBear(){
+//  document.getElementById("bearwalk1").style.backgroundPosition = `-96px 0px`;
+//}
 
 function Icebergg(x, y){
   this.x = x;
@@ -81,7 +136,7 @@ function startIcebergg(){
 
 }
 
-
+*/
 
 
 
@@ -105,10 +160,19 @@ function startgame(x){
     document.getElementById("polartitle").style.display = "none";
     clearInterval(notif);
     document.getElementById("startnotif").style.display = "none";
-    startBear();
-    startIcebergg();
+    bearAnimation();
+
+    //startBear();
+    //startIcebergg();
+    //if(x.keyCode == 38){
+    //  moveBearUp();
+    //  time.wait(0.2);
+    //  moveBearDown();
+
+    }
+
   }
-}
+
 var notif=setInterval(function(){
       $('blink').each(function() {
         $(this).toggle();
